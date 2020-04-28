80f58f2b67d0ecb494ed5a35d396950030d02f6c
Jacek <Jacek@BERLIN>
2020-04-28 08:09:57 +0200

    c

--- kurs7_sekcja3_jdbc-jpa/src/main/java/pl/jaceksysiak/database/demo/App.java
+++ kurs7_sekcja3_jdbc-jpa/src/main/java/pl/jaceksysiak/database/demo/App.java
@@ -1,4 +1,4 @@
-package pl.jaceksysiak.springboot.database.demo;
+package pl.jaceksysiak.database.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,14 +11,14 @@ public class App {
 	public static void main(String[] args) {
 		
 		//1. sposób
-		//SpringApplication.run(App.class, args);
+		SpringApplication.run(App.class, args);
 		
 		//2. sposób
-		ApplicationContext applicationContext = SpringApplication.run(App.class, args);
-		
-		for (String name : applicationContext.getBeanDefinitionNames()) {
-			System.out.println("applicationContext: " + name);
-		}
+//		ApplicationContext applicationContext = SpringApplication.run(App.class, args);
+//		
+//		for (String name : applicationContext.getBeanDefinitionNames()) {
+//			System.out.println("applicationContext: " + name);
+//		}
 	}
 
 }
--- kurs7_sekcja3_jdbc-jpa/src/main/java/pl/jaceksysiak/database/demo/Book.java
+++ kurs7_sekcja3_jdbc-jpa/src/main/java/pl/jaceksysiak/database/demo/Book.java
@@ -1,4 +1,4 @@
-package pl.jaceksysiak.springboot.database.demo;
+package pl.jaceksysiak.database.demo;
 
 public class Book {
 	
